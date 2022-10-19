3fc8e311dfda9ba2e1696c86e4a08ce53f3ad5e6
JohnWesleyPinto <92767143+JohnWesleyPinto@users.noreply.github.com>
2022-10-19 14:48:36 -0300

    Delete Main.java

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-
-public class Main {
-    public static void main(String[] args){
-        Tela tela = new Tela();
-        tela.setVisible(true);
-        tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-}
